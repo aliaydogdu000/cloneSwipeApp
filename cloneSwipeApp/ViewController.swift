ef9c8df6020cd3aa244e5308b1bfbe1d2829460d
Ali Aydoğdu <86653103+aliaydogdu000@users.noreply.github.com>
2023-07-11 00:29:15 +0300

    card added gesture controls set

--- cloneSwipeApp/ViewController.swift
+++ cloneSwipeApp/ViewController.swift
@@ -10,22 +10,32 @@ import UIKit
 class ViewController: UIViewController {
     
     let topStackView = TopNavigationStackView()
-    let blueView = UIView()
+    let cardsDeckView = UIView()
     let buttonStackView = HomeBottomControlsStackView()
-
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpLayout(cardsDeckView)
+        
+        setUpDummyCard()
+    }
+    
+    fileprivate func setUpDummyCard(){
+        let cardView = CardView(frame: .zero)
+        cardsDeckView.addSubview(cardView)
+        cardView.fillSuperview()
+    }
+    
     fileprivate func setUpLayout(_ blueView: UIView) {
         let overallStackView = UIStackView(arrangedSubviews: [topStackView,blueView,buttonStackView])
         overallStackView.axis = .vertical
         view.addSubview(overallStackView)
         overallStackView.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, trailing: view.trailingAnchor)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+        overallStackView.isLayoutMarginsRelativeArrangement = true
+        overallStackView.layoutMargins = .init(top:0,left:12,bottom: 0,right: 12)
         
-        blueView.backgroundColor = .blue
-        setUpLayout(blueView)
-
+        overallStackView.bringSubviewToFront(cardsDeckView)
     }
 
 
